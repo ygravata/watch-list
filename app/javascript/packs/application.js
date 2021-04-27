841a6fef5d40995f472084d117e616e97672ee26
Yuri Gravata <ygravata@gmail.com>
2021-04-27 11:01:59 -0300

    Add dotenv - Protect my secret data in .env file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,24 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import 'bootstrap';
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+import 'bootstrap';
+import 'select2/dist/css/select2.min.css'
+import 'jquery-bar-rating/dist/themes/css-stars';
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+import { initTyped } from './plugins/init_typedjs';
+import { initSelect2 } from './plugins/init_select2';
+import { initStarRating } from './plugins/init_star_rating';
+
+
+document.addEventListener('turbolinks:load', () => {
+  initSelect2();
+  initTyped();
+  AOS.init();
+  initStarRating();
+});
